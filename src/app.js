7da2fe5d3088564769dee069e4dd943d6be68a6f
Dipankar Ghosh <dgkgecece@gmail.com>
2026-08-01 00:48:11 +0530

    learned get, post and delete api

--- src/app.js
+++ src/app.js
@@ -10,9 +10,25 @@ app.get('/hello', (req, res) => {
   res.send('Server response for /hello!');
 });
 
+app.get('/hello/2', (req, res) => {
+  res.send('2 Server response for /hello!');
+});
+
 app.get('/test', (req, res) => {
   res.send('Server response for /test');
 });
+
+app.get('/user', (req, res) => {
+  res.send({ firstName: "Dipankar", lastName: "Ghosh"});
+})
+
+app.post('/user', (req, res)=>{
+  res.send('User data updated successfully!');
+})
+
+app.delete('/user', (req, res)=> {
+  res.send("User data deleted successfully");
+})
 
 app.listen(7000, ()=>{
     console.log("Server is running on http://localhost:7000");
