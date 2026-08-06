dc212e3225159b3a86af173564f9f16534eba277
Dipankar Ghosh <dgkgecece@gmail.com>
2026-08-06 23:31:18 +0530

    installed mongoose and connect to mongodb cluster

--- src/app.js
+++ src/app.js
@@ -2,34 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Server Home!');
-});
+const {connectDB} = require("./config/database");
 
-app.get('/hello', (req, res) => {
-  res.send('Server response for /hello!');
-});
-
-app.get('/hello/2', (req, res) => {
-  res.send('2 Server response for /hello!');
-});
-
-app.get('/test', (req, res) => {
-  res.send('Server response for /test');
-});
-
-app.get('/user', (req, res) => {
-  res.send({ firstName: "Dipankar", lastName: "Ghosh"});
-})
-
-app.post('/user', (req, res)=>{
-  res.send('User data updated successfully!');
-})
-
-app.delete('/user', (req, res)=> {
-  res.send("User data deleted successfully");
-})
-
-app.listen(7000, ()=>{
+connectDB()
+.then(() => {
+  console.log("DB connection estublished!!");
+  app.listen(7000, ()=>{
     console.log("Server is running on http://localhost:7000");
-});
+  });
+})
+.catch((err) => {
+    console.error("Database connection failed:", err.message);
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+// mongodb+srv://dgkgecece:<db_password>@cluster0.s5yjw.mongodb.net/
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://dgkgecece:0cvri2rt7v0MVfP4@cluster0.s5yjw.mongodb.net/");
+};
+
+module.exports = {connectDB};
