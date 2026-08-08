62cf328aa549e0e07210a8c08edfa996787e3755
Dipankar Ghosh <dgkgecece@gmail.com>
2026-08-08 20:01:09 +0530

    created /signup POST api with dynamic record from req.body

--- src/app.js
+++ src/app.js
@@ -1,22 +1,16 @@
 const express = require('express');
-
 const app = express();
 
 const {connectDB} = require("./config/database");
-const User = require('./module/user')
+const User = require('./module/user');
+app.use(express.json());
 
 app.post('/signup', async (req, res, next)=>{
-  const user = new User({
-    firstName: 'Spandita',
-    lastName: "pal",
-    email: 'spandita@pal.com',
-    password: 'cde@123',
-    age: 34,
-    gender: 'Female'
-  });
+  console.log(req.body);
+  const user = new User(req.body);
 
   try{
-    await User.save();
+    await user.save();
     res.send("User data saved successfully!!");
   }catch (err) {
     res.status(400).send('Error saving the user data : '+ err.message);
