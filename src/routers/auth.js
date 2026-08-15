9bd18b588b3b6c04f62e6d7a0d8cc1a881ca98cc
Dipankar Ghosh <dgkgecece@gmail.com>
2026-08-16 01:06:00 +0530

    logout api using express.Router() - invalidate cookie token

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -46,4 +46,12 @@ authRouter.post('/login', async (req, res)=>{
   }
 });
 
+authRouter.post('/logout', async (req, res) => {
+    res.cookie('token', null,
+        {
+            expires: new Date(Date.now())
+        })
+        .send('Logout successful!!!!');
+});
+
 module.exports = authRouter;
